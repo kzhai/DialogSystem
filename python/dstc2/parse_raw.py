d00af7df2f2c7ddc80c89132b6c984412765f1cb
Ke Zhai <kzhai@circlecar-lm.corp.yahoo.com>
2016-03-03 12:02:13 -0800

    update

--- python/dstc2/parse_raw.py
+++ python/dstc2/parse_raw.py
@@ -11,20 +11,21 @@ import json
 import sys
 import time
 
-import num2word;
-n2w = num2word.Num2Word();
+# import num2word;
+# n2w = num2word.Num2Word();
 
+'''
 time_pattern = re.compile(r'(\d+):(\d+)( [ap]m)?');
 ampm_pattern = re.compile(r' [ap] m?');
 post_route_pattern = re.compile(r'(\d+)(\D?)');
-pre_route_pattern= re.compile(r'(\D?)(\d+)');
+pre_route_pattern = re.compile(r'(\D?)(\d+)');
+'''
 multiple_spaces = re.compile(r' +');
-
-singleton_pattern = re.compile(r' [^ai] ');
+# singleton_pattern = re.compile(r' [^ai] ');
 
 month_dictionary = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
-def parse(input_directory, config_directory, output_directory, mapping_semantics=True):
+def parse(input_directory, config_directory, output_directory, mapping_semantics=False):
     train_list_file = os.path.join(config_directory, "dstc2_train.flist")
     train_output_file = os.path.join(output_directory, "train.dat")
     parse_data_split(input_directory, train_list_file, train_output_file, mapping_semantics);
@@ -40,7 +41,7 @@ def parse_data_split(input_directory, list_file_path, output_file_path, mapping_
         line = line.strip();
         dialog_file_path = os.path.join(input_directory, line, "log.json");
 
-        json_data=open(dialog_file_path);
+        json_data = open(dialog_file_path);
         data = json.load(json_data);
         
         session_id = data['session-id']
@@ -48,149 +49,102 @@ def parse_data_split(input_directory, list_file_path, output_file_path, mapping_
             turn = data['turns'][turn_index];
             
             assert "output" in turn, (session_id, turn_index, turn)
+
             if "transcript" not in turn["output"]:
                 sys.stderr.write("Missing transcript from system for session %s turn %d\n" % (session_id, turn_index));
                 system_transcript = "";
             else:
                 system_transcript = turn['output']['transcript'].lower().strip();
-                
-                if mapping_semantics:
-                    system_transcript = re.sub(time_pattern, "<time>", system_transcript);
-                    system_transcript = re.sub(post_route_pattern, " \\1 \\2 ", system_transcript);
-                    system_transcript = re.sub(pre_route_pattern, " \\1 \\2 ", system_transcript);
-                    system_transcript = re.sub(multiple_spaces, " ", system_transcript);
-                    
-                    value_slot_mapping = {};
-                    assert "dialog-acts" in turn['output']
-                    for act_index in xrange(len(turn['output']['dialog-acts'])):
-                        assert len(turn['output']['dialog-acts'][act_index])==2, (session_id, turn_index, turn)
-                        assert "slots" in turn['output']['dialog-acts'][act_index]
-                        #assert len(turn['output']['dialog-acts'][act_index]["slots"])<=1, turn['output']['dialog-acts'][act_index]["slots"]
-                        assert "act" in turn['output']['dialog-acts'][act_index]
-                        for slot_index in xrange(len(turn['output']['dialog-acts'][act_index]["slots"])):
-                            assert len(turn['output']['dialog-acts'][act_index]["slots"][slot_index])==2;
-                            
-                            if turn['output']['dialog-acts'][act_index]["slots"][slot_index][1]==None:
-                                continue;
-                            if turn['output']['dialog-acts'][act_index]["slots"][slot_index][0]=="act":
-                                continue;
-                            
-                            mapping_value = ("%s" % (turn['output']['dialog-acts'][act_index]["slots"][slot_index][1])).lower();
-                            mapping_slot = turn['output']['dialog-acts'][act_index]["slots"][slot_index][0];
-                            
-                            if mapping_value.isalpha() and mapping_slot=="route":
-                                continue;
-                            
-                            if "time" in mapping_slot:
-                                mapping_slot = "<time> ";
-                            elif "date" in mapping_slot:
-                                if "absmonth" in mapping_slot:
-                                    mapping_value = month_dictionary[int(mapping_value)-1];
-                                mapping_slot = "<date> ";
-                            else:
-                                mapping_slot = "<%s> " % mapping_slot.split(".")[-1];
-                            
-                            if (mapping_value in value_slot_mapping) and (value_slot_mapping[mapping_value]!=mapping_slot):
-                                print "duplicated mappings:", session_id, turn_index, mapping_value, value_slot_mapping[mapping_value], mapping_slot
-                            else:
-                                value_slot_mapping[mapping_value] = mapping_slot;
-                                
-                                mapping_value = re.sub(post_route_pattern, " \\1 \\2 ", mapping_value).strip();
-                                mapping_value = re.sub(pre_route_pattern, " \\1 \\2 ", mapping_value).strip();
-                                mapping_value = re.sub(multiple_spaces, " ", mapping_value);
-                                value_slot_mapping[mapping_value] = mapping_slot
-                                
-                                text_mapping_value = " ";
-                                for mapping_value_token in ("%s" % mapping_value).split():
-                                    if mapping_value_token.isdigit():
-                                        text_mapping_value += "%s " % n2w.to_cardinal(int(mapping_value_token));
-                                    else:
-                                        text_mapping_value += "%s " % mapping_value_token;
-                                text_mapping_value = text_mapping_value.strip();
-                                if len(text_mapping_value)>0:
-                                    if text_mapping_value=="y one":
-                                        text_mapping_value = " %s" % text_mapping_value;
-                                        mapping_slot = " %s" % mapping_slot
-                                    value_slot_mapping[text_mapping_value] = mapping_slot
-                                    
-                    for mapping_value in sorted(value_slot_mapping, key=lambda key: len("%s" % key), reverse=True):
-                        system_transcript = system_transcript.replace(mapping_value, value_slot_mapping[mapping_value]);
-                
-                system_transcript = re.sub(singleton_pattern, " ", system_transcript);    
                 system_transcript = re.sub(multiple_spaces, " ", system_transcript);
+                
+                system_acts = set();
+                
+                value_slot_mapping = {};
+                assert "dialog-acts" in turn['output']
+                # assert len(turn['output']['dialog-acts']) == 1, turn['output']['dialog-acts'];
+                for act_index in xrange(len(turn['output']['dialog-acts'])):
+                    assert len(turn['output']['dialog-acts'][act_index]) == 2, (session_id, turn_index, turn)
+                    
+                    assert "act" in turn['output']['dialog-acts'][act_index]
+                    system_act = turn['output']['dialog-acts'][act_index]["act"].strip();
+                    assert len(system_act.split())==1
+                    system_acts.add(system_act);
+                    
+                    assert "slots" in turn['output']['dialog-acts'][act_index]
+                    
+                    if mapping_semantics:
+                        for slot_index in xrange(len(turn['output']['dialog-acts'][act_index]["slots"])):
+                            assert len(turn['output']['dialog-acts'][act_index]["slots"][slot_index]) == 2;
 
+                            mapping_slot = turn['output']['dialog-acts'][act_index]["slots"][slot_index][0].lower().strip();
+                            assert len(mapping_slot.split()) == 1;
+                            mapping_slot = "<%s>" % mapping_slot;
+
+                            mapping_value = turn['output']['dialog-acts'][act_index]["slots"][slot_index][1].lower().strip();
+                            # mapping_value = ("%s" % (turn['output']['dialog-acts'][act_index]["slots"][slot_index][1])).lower();
+                            mapping_value = re.sub(multiple_spaces, " ", mapping_value);
+                            
+                            if (mapping_value in value_slot_mapping) and (value_slot_mapping[mapping_value] != mapping_slot):
+                                print "duplicated mappings from value to slot:", session_id, turn_index, mapping_value, value_slot_mapping[mapping_value], mapping_slot
+                            else:
+                                value_slot_mapping[mapping_value] = mapping_slot
+                                    
+                for mapping_value in sorted(value_slot_mapping, key=lambda key: len("%s" % key), reverse=True):
+                    system_transcript = system_transcript.replace(mapping_value, value_slot_mapping[mapping_value]);
+                
+                # system_transcript = re.sub(singleton_pattern, " ", system_transcript);    
+                system_transcript = re.sub(multiple_spaces, " ", system_transcript);
+                
             assert "input" in turn, (session_id, turn_index, turn)
             assert "live" in turn['input'], (session_id, turn_index, turn['input'])
             assert "asr-hyps" in turn['input']['live'], (session_id, turn_index, turn['input']['live'])
             assert "slu-hyps" in turn['input']['live'], (session_id, turn_index, turn['input']['live'])
 
-            if len(turn['input']['live']['asr-hyps'])<1:
+            if len(turn['input']['live']['asr-hyps']) < 1:
                 sys.stderr.write("Missing transcript from user for session %s turn %d\n" % (session_id, turn_index));
                 user_transcript = ""
             else:
                 user_transcript = turn['input']['live']['asr-hyps'][0]["asr-hyp"].lower().strip();
+                user_transcript = re.sub(multiple_spaces, " ", user_transcript);
                 
-                if mapping_semantics:
-                    user_transcript = re.sub(time_pattern, "<time>", user_transcript);
-                    user_transcript = re.sub(post_route_pattern, " \\1 \\2 ", user_transcript);
-                    user_transcript = re.sub(pre_route_pattern, " \\1 \\2 ", user_transcript);
-                    user_transcript = re.sub(multiple_spaces, " ", user_transcript);
-
-                    value_slot_mapping = {};
+                user_acts = set();
+                value_slot_mapping = {};
+                
+                for slu_hyp_index in xrange(len(turn['input']['live']['slu-hyps'][0]['slu-hyp'])):
+                    assert len(turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]) == 2, (session_id, turn_index);
                     
-                    '''
-                    if len(turn['input']['live']['slu-hyps'][0]['slu-hyp'])==0:
-                        continue;
+                    assert "act" in turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]
+                    user_act = turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['act'].strip()
+                    assert len(user_act.split())==1;
+                    user_acts.add(user_act);
                     
-                    print dialog_file_path, type(turn['input']['live']['slu-hyps'][0]['slu-hyp']), len(turn['input']['live']['slu-hyps'][0]['slu-hyp']), turn['input']['live']['slu-hyps'][0]['slu-hyp']
-                    assert len(turn['input']['live']['slu-hyps'][0]['slu-hyp'])==1, (session_id, turn_index);
-                    '''
+                    assert "slots" in turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]
                     
-                    for slu_hyp_index in xrange(len(turn['input']['live']['slu-hyps'][0]['slu-hyp'])):
-                        assert len(turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index])==2, (session_id, turn_index);
+                    if mapping_semantics:
                         for slot_index in xrange(len(turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'])):
-                            assert len(turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index])==2;
-                                
-                            if turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][1]==None:
+                            assert len(turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index]) == 2;
+
+                            mapping_slot = turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][0].lower().strip();
+                            assert len(mapping_slot.split()) == 1;
+                            if mapping_slot == "this":
                                 continue;
-                            if turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][1]=="act":
-                                continue;
-                                
-                            mapping_value = ("%s" % turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][1]).lower();
-                            mapping_slot = turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][0];
-                            #print mapping_slot, mapping_value
+                            mapping_slot = "<%s>" % mapping_slot;
                             
-                            if mapping_slot=="this":
-                                continue;
-                            mapping_slot = "<%s> " % mapping_slot;
-    
-                            if (mapping_value in value_slot_mapping) and (value_slot_mapping[mapping_value]!=mapping_slot):
+                            mapping_value = turn['input']['live']['slu-hyps'][0]['slu-hyp'][slu_hyp_index]['slots'][slot_index][1].lower().strip();
+                            mapping_value = re.sub(multiple_spaces, " ", mapping_value);
+                            
+                            if (mapping_value in value_slot_mapping) and (value_slot_mapping[mapping_value] != mapping_slot):
                                 print session_id, turn_index, value_slot_mapping[mapping_value], mapping_slot, mapping_value
                             else:
                                 value_slot_mapping[mapping_value] = mapping_slot;
                                 
-                                mapping_value = re.sub(post_route_pattern, " \\1 \\2 ", mapping_value).strip();
-                                mapping_value = re.sub(pre_route_pattern, " \\1 \\2 ", mapping_value).strip();
-                                mapping_value = re.sub(multiple_spaces, " ", mapping_value);
-                                value_slot_mapping[mapping_value] = mapping_slot
-    
-                                text_mapping_value = "";
-                                for mapping_value_token in mapping_value.split():
-                                    if mapping_value_token.isdigit():
-                                        text_mapping_value += "%s " % n2w.to_cardinal(int(mapping_value_token));
-                                    else:
-                                        text_mapping_value += "%s " % mapping_value_token;
-                                text_mapping_value = text_mapping_value.strip();
-                                if len(text_mapping_value)>0:
-                                    value_slot_mapping[text_mapping_value] = mapping_slot
-                        
-                    for mapping_value in sorted(value_slot_mapping, key=lambda key: len("%s" % key), reverse=True):
-                        user_transcript = user_transcript.replace(mapping_value, value_slot_mapping[mapping_value]);
+                for mapping_value in sorted(value_slot_mapping, key=lambda key: len("%s" % key), reverse=True):
+                    user_transcript = user_transcript.replace(mapping_value, value_slot_mapping[mapping_value]);
                     
-                user_transcript = re.sub(singleton_pattern, " ", user_transcript);
+                # user_transcript = re.sub(singleton_pattern, " ", user_transcript);
                 user_transcript = re.sub(multiple_spaces, " ", user_transcript);
 
-            output_stream.write("%s\t%d\t%s\t%s\n" % (session_id, turn_index, system_transcript, user_transcript));
+            output_stream.write("%s\t%d\t%s\t%s\t%s\t%s\n" % (session_id, turn_index, system_transcript, " ".join(system_acts), user_transcript, " ".join(user_acts)));
             
         json_data.close()
                 
